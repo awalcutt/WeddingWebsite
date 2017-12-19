492bf32a7e441b7185dc2c1d968824d90b30426f
Alex Walcutt <RobotChicken34@gmail.com>
2017-12-18 20:18:51 -0800

    Redirect all HTTP requests to the HTTPS endpoint

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,43 @@ extern crate maud;
 extern crate rocket;
 
 use maud::{html, Markup};
+use rocket::{request, Outcome, Request, Rocket};
+use rocket::http::Status;
+use rocket::request::FromRequest;
+use rocket::response::Redirect;
+
+static HOSTNAME: &'static str = "https://alexlillian.wedding";
+static X_FORWARDED_PROTO: &'static str = "X-Forwarded-Proto";
+
+struct Https;
+
+impl<'a, 'r> FromRequest<'a, 'r> for Https {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let client_protocol_headers: Vec<&str> = request.headers().get(X_FORWARDED_PROTO).collect();
+
+        if client_protocol_headers.len() != 1 {
+            return Outcome::Failure((Status::UpgradeRequired, ()))
+        }
+
+        let client_protocol = client_protocol_headers[0];
+
+        match client_protocol {
+            "https" => Outcome::Success(Https),
+            _ => Outcome::Failure((Status::UpgradeRequired, ()))
+        }
+    }
+}
+
+#[error(426)]
+fn http_redirect(request: &Request) -> Redirect {
+    let redirect_uri = HOSTNAME.to_owned() + request.uri().path();
+    Redirect::permanent(&redirect_uri)
+}
 
 #[get("/")]
-fn index() -> Markup {
+fn index(_https: Https) -> Markup {
     html! {
         link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
         meta name="viewport" content="width=device-width, initial-scale=1";
@@ -35,6 +69,41 @@ fn index() -> Markup {
     }
 }
 
+fn rocket() -> Rocket {
+    rocket::ignite().mount("/", routes![index]).catch(errors![http_redirect])
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Header;
+    use rocket::http::Status;
+
+    use super::X_FORWARDED_PROTO;
+
+    #[test]
+    fn index_https_ok() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.get("/").header(Header::new(X_FORWARDED_PROTO, "https")).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+
+    #[test]
+    fn index_http_redirect() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.get("/").header(Header::new(X_FORWARDED_PROTO, "http")).dispatch();
+        assert_eq!(response.status(), Status::PermanentRedirect);
+    }
+
+    #[test]
+    fn index_headerless_redirect() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::PermanentRedirect);
+    }
 }
