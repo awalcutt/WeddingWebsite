a4db7a5f671e028c8ecccfc76f7ec661eca0d04c
Alex Walcutt <RobotChicken34@gmail.com>
2018-07-16 21:57:08 -0700

    Update childcare text

--- src/page/childcare.rs
+++ src/page/childcare.rs
@@ -10,7 +10,7 @@ impl WeddingWebsitePage for ChildcarePage {
         html! {
             div.jumbotron {
                 p "We don't want anyone to feel left out of the festivities including parents and children. That is why we are pleased to share that we will have childcare services at the reception!"
-                p "At the reception venue (Panoramic Room at Pacific Tower) children can hang out right down the hall from the main room along with a licensed childcare provider."
+                p "At the reception venue (Panoramic Room at Pacific Tower) children can hang out right down the hall from the main room along with a trusted childcare provider."
                 p "There will not be childcare services at the ceremony venue (Ella Bailey Park) but it is a public park with a playground in case children (or adults) get restless."
                 p "Please let us known on your RSVP how many children will be attending and reach out to Lillian or Alex if you have any questions."
             }
